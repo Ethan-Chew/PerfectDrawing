520090745ddfbb6ac05693e786631bea6e749abe
Ethan Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2022-06-05 11:28:31 +0800

    Initial Commit

---
+++ PerfectDrawing/PerfectDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectDrawingApp.swift
+//  PerfectDrawing
+//
+//  Created by Ethan Chew on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct PerfectDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
